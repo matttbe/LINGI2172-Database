414e67eebd28b695234a2c60a9dfc6c0d8365c3f
ursula <benoit.baufays@conceptbandb.be>
2014-04-25 20:58:21 +0200

    add login method

--- workspace/CharlyParkingApps/src/com/charlyparkingapps/CharlyApplication.java
+++ workspace/CharlyParkingApps/src/com/charlyparkingapps/CharlyApplication.java
@@ -1,12 +1,22 @@
 package com.charlyparkingapps;
 
+import com.charlyparkingapps.db.UserDB;
 import com.charlyparkingapps.db.object.User;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 
 public class CharlyApplication extends Application{
 
     private User current_user;
+    
+    @Override
+	public void onCreate() {
+    	
+    }
 
 	public User getCurrent_user() {
 		return current_user;
@@ -14,6 +24,19 @@ public class CharlyApplication extends Application{
 
 	public void setCurrent_user(User current_user) {
 		this.current_user = current_user;
+	}
+	
+	public boolean testLogIn(Activity activity){
+		SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+		int id = sharedPref.getInt(getString(R.string.id_user_pref), 0);
+		if(id!=0){
+			UserDB user = new UserDB(getApplicationContext());
+			user.Open();
+			((CharlyApplication) getApplicationContext()).setCurrent_user(user.GetById(id));
+			user.Close();
+			return true;
+		}
+		return false;
 	}
     
 
