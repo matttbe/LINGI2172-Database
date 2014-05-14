040c014d511e7c68b273be04a4b989d27ac4ba00
Alex Vermeylen <alex@localhost.localdomain>
2014-05-15 00:59:20 +0200

    user friendly toast to say that car edit modifs are saved when the user go back

--- workspace/CharlyParkingApps/src/com/charlyparkingapps/CarEditActivity.java
+++ workspace/CharlyParkingApps/src/com/charlyparkingapps/CarEditActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.charlyparkingapps.db.object.Car;
 
@@ -97,6 +98,9 @@ public class CarEditActivity extends Activity implements
 		if (!mRemoved) {
 			// TODO : add modifications to the db !
 		}
+		Toast saved = Toast.makeText(this, R.string.modifs_saved,
+				Toast.LENGTH_LONG);
+		saved.show();
 		super.onBackPressed();
 	}
 
